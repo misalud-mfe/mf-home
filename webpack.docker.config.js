4793dde1c697793e964387a2c064fc53c58dbd45
haroldevs <haroldthomasg18@gmail.com>
2025-06-02 01:40:13 -0500

    chore: docker

--- webpack.docker.config.js
+++ webpack.docker.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const { plugins } = require("../moneyExchange/webpack.config");
 const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = {
@@ -8,8 +9,12 @@ module.exports = {
   devServer: {
     port: 3000,
     host: "0.0.0.0",
-    static: path.resolve(__dirname, "dist"),
     allowedHosts: "all",
+    static: path.resolve(__dirname, "dist"),
+    historyApiFallback: {
+      index: "/index.html", // redirige todas las rutas al archivo principal
+    },
+    
   },
   output: {
     publicPath: "auto",
@@ -23,19 +28,45 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.module\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        exclude: /\.module\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "dashboard",
+      name: "home",
       remotes: {
         atendidos: "atendidos@http://localhost:3001/remoteEntry.js",
         pendientes: "pendientes@http://localhost:3002/remoteEntry.js",
         transferencias: "transferencias@http://localhost:3003/remoteEntry.js",
+        login: "login@http://localhost:3004/remoteEntry.js",
       },
       shared: {
         react: { singleton: true, eager: true },
         "react-dom": { singleton: true, eager: true },
+        "common-utils": {
+          singleton: true,
+          eager: true,
+        },
+        rxjs: {
+          singleton: true,
+          eager: true,
+        },
       },
     }),
     new HtmlWebpackPlugin({
